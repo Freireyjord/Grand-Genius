0374ddc256126196d3ca967fe46f2f3b7a407d7a
João Freire <106822699+Freireyjord@users.noreply.github.com>
2023-06-14 18:06:44 -0300

    Update G_G.c
    mudanças intermediarias 
    gera uma sequencia de 1 cor 
    impossivel perder (nao gera outra sequencia apos a animação de derrota)

--- G_G.c
+++ G_G.c
@@ -1,79 +1,34 @@
 #include <msp430.h>
 #include <stdlib.h>
 
-#define BUTr BIT4
-#define BUTg BIT5
-#define BUTb BIT6
-#define BUTy BIT7
+#define LEDy BIT0    // define o pino de saída do LED como P1.0
+#define LEDr BIT1    // define o pino de saída do LED como P1.0
+#define LEDb BIT2    // define o pino de saída do LED como P1.0
+#define LEDg BIT3    // define o pino de saída do LED como P1.0
 
-#define LEDr BIT0
-#define LEDg BIT1
-#define LEDb BIT2
-#define LEDy BIT3
+#define BUTy BIT5    // define o pino de saída do LED como P1.4
+#define BUTb BIT4    // define o pino de saída do LED como P1.4
+#define BUTr BIT1    // define o pino de saída do LED como P1.4
+#define BUTg BIT0    // define o pino de saída do LED como P1.4
 
-#define DELAY_CYCLES 100000
+#define DELAY_CYCLES 300000
 
 int sequence[100];
-int sequence_length = 0;
+int sequence_length = 1;
 int current_index = 0;
 int game_over = 0;
 
-void initialize()
+int check_button_press1(int button)
 {
-    WDTCTL = WDTPW + WDTHOLD; // Desabilitar o Watchdog Timer
-    P1DIR |= (LEDr + LEDg + LEDb + LEDy); // Definir pinos dos LEDs como saída
-    P1OUT &= ~(LEDr + LEDg + LEDb + LEDy); // Desligar todos os LEDs
-    P1REN |= (BUTr + BUTg + BUTb + BUTy); // Habilitar resistor de pull-up interno para os botões
-    P1IE |= (BUTr + BUTg + BUTb + BUTy); // Habilitar interrupção para os botões
-    P1IES |= (BUTr + BUTg + BUTb + BUTy); // Definir interrupção para borda de descida
-    P1IFG &= ~(BUTr + BUTg + BUTb + BUTy); // Limpar as flags de interrupção dos botões
-    __enable_interrupt(); // Habilitar interrupções gerais
-}
-
-void generate_next_sequence()
-{
-    int i;
-    for (i = 0; i < sequence_length; i++) {
-        sequence[i] = rand() % 4;
+    if (P1IFG & button) {
+        return 0;
+    } else {
+        return 1;
     }
 }
-
-void show_sequence()
+int check_button_press2(int button)
 {
-    int i;
-    for (i = 0; i < sequence_length; i++) {
-        switch (sequence[i]) {
-            case 0:
-                P1OUT |= LEDr;
-                __delay_cycles(DELAY_CYCLES);
-                P1OUT &= ~LEDr;
-                __delay_cycles(DELAY_CYCLES);
-                break;
-            case 1:
-                P1OUT |= LEDg;
-                __delay_cycles(DELAY_CYCLES);
-                P1OUT &= ~LEDg;
-                __delay_cycles(DELAY_CYCLES);
-                break;
-            case 2:
-                P1OUT |= LEDb;
-                __delay_cycles(DELAY_CYCLES);
-                P1OUT &= ~LEDb;
-                __delay_cycles(DELAY_CYCLES);
-                break;
-            case 3:
-                P1OUT |= LEDy;
-                __delay_cycles(DELAY_CYCLES);
-                P1OUT &= ~LEDy;
-                __delay_cycles(DELAY_CYCLES);
-                break;
-        }
-    }
-}
-
-int check_button_press(int button)
-{
-    if (P1IN & button) {
+    if (P2IFG & button) {
         return 0;
     } else {
         return 1;
@@ -92,85 +47,146 @@ void game_over_sequence()
     game_over = 1;
 }
 
-void button_press(int button)
+void next_sequence()
 {
-    switch (button) {
-        case 0:
-            P1OUT |= LEDr;
-            __delay_cycles(DELAY_CYCLES);
-            P1OUT &= ~LEDr;
-            __delay_cycles(DELAY_CYCLES);
-            break;
-        case 1:
+
+        int i;
+        for (i = 0; i < sequence_length; i++) {
+            sequence[i] = rand() % 4;
+        }
+
+}
+
+int main(void) {
+
+    WDTCTL = WDTPW | WDTHOLD;       //WATCHDOG TIMER PARADO
+    srand(1);
+
+        P1DIR |= (LEDr + LEDg + LEDb + LEDy); // Definir pinos dos LEDs como saída
+        P1OUT &= ~(LEDr + LEDg + LEDb + LEDy); // Desligar todos os LEDs
+
+        P1REN |= (BUTb + BUTy); // Habilitar resistor de pull-up interno para os botões
+        P1IE |= (BUTb + BUTy); // Habilitar interrupção para os botões
+        P1IES |= (BUTb + BUTy); // Definir interrupção para borda de descida
+        P1IFG &= ~(BUTb + BUTy); // Limpar as flags de interrupção dos botões
+
+        P2REN |= (BUTr + BUTg); // Habilitar resistor de pull-up interno para os botões
+        P2IE |= (BUTr + BUTg); // Habilitar interrupção para os botões
+        P2IES |= (BUTr + BUTg); // Definir interrupção para borda de descida
+        P2IFG &= ~(BUTr + BUTg); // Limpar as flags de interrupção dos botões
+
+
+        sequence[0] = rand() % 4;
+
+
+        switch (sequence[0]) {
+                            case 0:
+                                P1OUT |= LEDr;
+                                __delay_cycles(DELAY_CYCLES);
+                                P1OUT &= ~LEDr;
+                                __delay_cycles(DELAY_CYCLES);
+                                break;
+                            case 1:
+                                P1OUT |= LEDg;
+                                __delay_cycles(DELAY_CYCLES);
+                                P1OUT &= ~LEDg;
+                                __delay_cycles(DELAY_CYCLES);
+                                break;
+                            case 2:
+                                P1OUT |= LEDb;
+                                __delay_cycles(DELAY_CYCLES);
+                                P1OUT &= ~LEDb;
+                                __delay_cycles(DELAY_CYCLES);
+                                break;
+                            case 3:
+                                P1OUT |= LEDy;
+                                __delay_cycles(DELAY_CYCLES);
+                                P1OUT &= ~LEDy;
+                                __delay_cycles(DELAY_CYCLES);
+                                break;
+        }
+
+    __enable_interrupt();
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void port1_int(void){
+
+    if (check_button_press1(BUTy)) {
+        P1OUT |= LEDy;
+        __delay_cycles(DELAY_CYCLES);
+        P1OUT &= ~LEDy;
+        __delay_cycles(DELAY_CYCLES);
+
+        if(sequence[current_index] == 3){
+            current_index += 1;
+            if(current_index == sequence_length){
+                current_index = 0;
+                sequence_length += 1;
+                next_sequence();
+            }
+        }else{
+            game_over_sequence();
+        }
+
+    }
+    if (check_button_press1(BUTb)) {
+        P1OUT |= LEDb;
+        __delay_cycles(DELAY_CYCLES);
+        P1OUT &= ~LEDb;
+        __delay_cycles(DELAY_CYCLES);
+
+        if(sequence[current_index] == 2){
+            current_index += 1;
+            if(current_index == sequence_length){
+                current_index = 0;
+                sequence_length += 1;
+                next_sequence();
+            }
+        }else{
+            game_over_sequence();
+        }
+
+    }
+
+    P1IFG &= ~(BUTb + BUTy);     //LIMPA FLAG DE INTERRUPÇÃO DO PINO P1.3
+}
+
+#pragma vector = PORT2_VECTOR
+__interrupt void port2_int(void){
+
+    if (check_button_press2(BUTr)) {
+        P1OUT |= LEDr;
+        __delay_cycles(DELAY_CYCLES);
+        P1OUT &= ~LEDr;
+        __delay_cycles(DELAY_CYCLES);
+        if(sequence[current_index] == 0){
+                    current_index += 1;
+                    if(current_index == sequence_length){
+                        current_index = 0;
+                        sequence_length += 1;
+                        next_sequence();
+                    }
+                }else{
+                    game_over_sequence();
+                }
+    }
+    if (check_button_press2(BUTg)) {
             P1OUT |= LEDg;
             __delay_cycles(DELAY_CYCLES);
             P1OUT &= ~LEDg;
             __delay_cycles(DELAY_CYCLES);
-            break;
-        case 2:
-            P1OUT |= LEDb;
-            __delay_cycles(DELAY_CYCLES);
-            P1OUT &= ~LEDb;
-            __delay_cycles(DELAY_CYCLES);
-            break;
-        case 3:
-            P1OUT |= LEDy;
-            __delay_cycles(DELAY_CYCLES);
-            P1OUT &= ~LEDy;
-            __delay_cycles(DELAY_CYCLES);
-            break;
-    }
-}
-
-int main(void)
-{
-    initialize();
-    srand(1); // Inicializar o gerador de números aleatórios
-
-    while (1) {
-        if (game_over) {
-            __delay_cycles(DELAY_CYCLES * 5);
-            sequence_length = 0;
-            current_index = 0;
-            game_over = 0;
-            generate_next_sequence();
-            show_sequence();
-        } else {
-            if (current_index == sequence_length) {
-                sequence_length++;
-                current_index = 0;
-                show_sequence();
-            }
-
-            if (check_button_press(BUTTON1)) {
-                button_press(0);
-                if (sequence[current_index] != 0) {
-                    game_over_sequence();
-                } else {
-                    current_index++;
-                }
-            } else if (check_button_press(BUTTON2)) {
-                button_press(1);
-                if (sequence[current_index] != 1) {
-                    game_over_sequence();
-                } else {
-                    current_index++;
-                }
-            } else if (check_button_press(BUTTON3)) {
-                button_press(2);
-                if (sequence[current_index] != 2) {
-                    game_over_sequence();
-                } else {
-                    current_index++;
-                }
-            } else if (check_button_press(BUTTON4)) {
-                button_press(3);
-                if (sequence[current_index] != 3) {
-                    game_over_sequence();
-                } else {
-                    current_index++;
-                }
-            }
+            if(sequence[current_index] == 1){
+                        current_index += 1;
+                        if(current_index == sequence_length){
+                            current_index = 0;
+                            sequence_length += 1;
+                            next_sequence();
+                        }
+                    }else{
+                        game_over_sequence();
+                    }
         }
-    }
+
+    P2IFG &= ~(BUTr + BUTg);     //LIMPA FLAG DE INTERRUPÇÃO DO PINO P1.3
 }
